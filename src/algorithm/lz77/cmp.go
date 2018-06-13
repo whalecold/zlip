6b2fa90fcd959be1f7f46e2b8dbb21a5374f2789
zhenglisheng <zls1129@gmail.com>
2018-06-13 13:30:54 +0800

    实现部分lz77编码

--- src/algorithm/lz77/cmp.go
+++ src/algorithm/lz77/cmp.go
@@ -1,38 +1,132 @@
 package lz77
 
 import (
-	"hash"
-	"encoding/binary"
 	"fmt"
-	"crypto/sha1"
 )
 
 
-var hashIns hash.Hash
-
 func genHashNumber(bytes []byte) uint16 {
-	ret := hashIns.Sum(bytes)
-	ret = ret[:8]
-	fmt.Printf("%v   %v  %x\n", bytes, ret, binary.LittleEndian.Uint64(ret))
-	return uint16(binary.LittleEndian.Uint64(ret) & uint64(LZ77_WindowsMask))
+	var hash uint32
+	hash = uint32(bytes[0]) << 16 + uint32(bytes[1]) << 8 + uint32(bytes[2])
+	//fmt.Printf("%v\n", uint16(hash & LZ77_WindowsMask))
+	return uint16(hash & LZ77_WindowsMask)
+}
+
+//查看最长匹配串
+func checkLargestCmpBytes(bytes []byte, curIndex , cmpIndex, maxSize uint64) uint64 {
+	//fmt.Printf("cur Index %v cmpIndex %v\n", curIndex, cmpIndex)
+	var length uint64
+	temp := curIndex
+	for {
+		if curIndex >= maxSize || bytes[curIndex] != bytes[cmpIndex] || cmpIndex >= temp  {
+			break
+		}
+		curIndex++
+		cmpIndex++
+		length++
+	}
+	//匹配的长度不要超过这个值
+	if length >= LZ77_MaxCmpLength {
+		return 0
+	}
+	return length
+}
+
+//更新prev和head的索引
+func updateHashIndex(prev, head []uint64, hash uint16, index uint64) {
+	temp := head[hash]
+	head[hash] = index
+	if temp != 0 {
+		prev[index & LZ77_WindowsMask] = temp
+	}
+}
+
+//更新bytes数组的前三位hash值
+func updateHashBytes(bytes []byte, index uint64, prev, head []uint64) uint16 {
+	//这里是更新接下来的匹配
+	hash := genHashNumber(bytes[index-LZ77_MinCmpSize:index])
+	updateHashIndex(prev, head, hash, index-LZ77_MinCmpSize)
+
+	return hash
 }
 
 //这个是匹配是算法
-
-func Cmp(bytes []byte) {
-	if len(bytes) < 3 {
+//因为len(bytes)返回的是int 文件大小可能超过 所以长度用新的uint64参数表示
+func lz77Cmp(bytes []byte, size uint64) []byte {
+	if len(bytes) < LZ77_MinCmpSize * 2 {
 		panic("func cmp bytes need large than 3")
 	}
-	hashIns = sha1.New()
-	index := genHashNumber(bytes[:3])
 
-	fmt.Printf(" hash %v \n", index)
+	result := make([]byte, 0, 1024)
+	result = append(result, bytes[:LZ77_MinCmpSize]...)
+	prevIndex := make([]uint64, LZ77_CmpPrevSize)
+	headIndex := make([]uint64, LZ77_CmpHeadSize)
 
-	//prevIndex := make([]uint64, LZ77_CmpPrevSize)
-	//headIndex := make([]uint64, LZ77_CmpPrevSize)
-	//
-	////小于三个字节
-	//for i := 3; i < len(bytes); i++ {
-	//
-	//}
+
+	//小于三个字节
+	var index uint64
+	for index = LZ77_MinCmpSize; index + LZ77_MinCmpSize <= size;  {
+
+		//每次移动窗口都要更新值
+		updateHashBytes(bytes, index, prevIndex, headIndex)
+
+		hash := genHashNumber(bytes[index:index + LZ77_MinCmpSize])
+		cmpIndex := headIndex[hash]
+		if cmpIndex == 0 { //没有匹配到
+			result = append(result, bytes[index])
+			index++
+		} else {	//匹配到了
+
+			var maxCmpStart uint64
+			var maxCmpLength uint64
+			var maxCmpNumber uint64
+			//遍历hash值一致的链表
+			for {
+
+				length := checkLargestCmpBytes(bytes, index, cmpIndex, size)
+
+				if maxCmpLength < length && index-cmpIndex < LZ77_MaxWindowsSize{
+				//if maxCmpLength < length {
+				//	fmt.Printf("distance %v\n", index-maxCmpStart)
+					maxCmpLength = length
+					maxCmpStart = cmpIndex
+
+				}
+
+				cmpIndex = prevIndex[cmpIndex & LZ77_WindowsMask]
+				if cmpIndex == 0 {
+					break
+				}
+				//fmt.Printf("luup %v %v\n", cmpIndex, prevIndex[cmpIndex & LZ77_WindowsMask])
+				maxCmpNumber++
+				//限制匹配次数 不做判断会造成死循环
+				if maxCmpNumber >= LZ77_MaxCmpNum {
+					break
+				}
+			}
+
+			//还是没有匹配到 或者 匹配到的是hash冲突的字段
+			if maxCmpLength < LZ77_MinCmpSize {
+				result = append(result, bytes[index])
+				index++
+			} else {
+				str := fmt.Sprintf("(%v,%v)", maxCmpLength, index-maxCmpStart)
+				temp := index + 1
+				index += maxCmpLength
+
+				for ; temp < index; temp++ {
+					updateHashBytes(bytes, temp, prevIndex, headIndex)
+				}
+				result = append(result, []byte(str)...)
+			}
+		}
+	}
+
+	//如果还有剩余 直接打印出来 不匹配了
+	for ; index < size; index++ {
+		result = append(result, bytes[index])
+	}
+	//fmt.Printf("prev %v\n", prevIndex)
+	//fmt.Printf("head %v\n", headIndex)
+	return result
 }
--- src/algorithm/lz77/parma.go
+++ src/algorithm/lz77/parma.go
@@ -1,8 +1,12 @@
 package lz77
 
 const (
-	LZ77_MaxWindowsSize = 1024 * 32		//滑动窗口的大小
+	//LZ77_MaxWindowsSize =  32 * 1024		//滑动窗口的大小
+	LZ77_MaxWindowsSize =  32 		//滑动窗口的大小
 	LZ77_CmpHeadSize 	= LZ77_MaxWindowsSize
 	LZ77_CmpPrevSize 	= LZ77_MaxWindowsSize
-	LZ77_WindowsMask 	= 0x7FFF
+	LZ77_WindowsMask 	= LZ77_MaxWindowsSize - 1
+	LZ77_MinCmpSize 	= 3
+	LZ77_MaxCmpNum 		= 16 					//最大比较次数
+	LZ77_MaxCmpLength 	= 256 					//最大比较长度
 )
